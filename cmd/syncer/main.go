b81c3f3915d6ffe5396808632a544fe0fcb82234
Seiji Fouquet <seiji@adikteev.com>
2024-06-24 16:14:41 +0200

    feat: add support for multiple Route53 Zones

--- cmd/syncer/main.go
+++ cmd/syncer/main.go
@@ -17,7 +17,7 @@ type config struct {
 	grpc      int
 	http      int
 	timeout   int
-	zoneID    string
+	zoneIDs   string
 	services  string
 	interval  int
 }
@@ -35,17 +35,18 @@ func main() {
 	flag.IntVar(&conf.http, "consul-http-port", 8500, "http port of consul server")
 	flag.IntVar(&conf.timeout, "consul-http-timeout", 5, "http timeout for connecting to consul server")
 	flag.StringVar(&conf.services, "consul-services", "", "comma separated consul services to synchronize [REQUIRED]")
-	flag.StringVar(&conf.zoneID, "dns-zone-id", "", "route53 zone-ID to synchronize to [REQUIRED]")
+	flag.StringVar(&conf.zoneIDs, "dns-zone-ids", "", "comma separated route53 zone-ID's to synchronize [REQUIRED]")
 	flag.IntVar(&conf.interval, "refresh-interval", 20, "interval between sync")
 	flag.Parse()
 
-	if conf.addresses == "" && conf.services == "" || conf.zoneID == "" {
+	if conf.addresses == "" && conf.services == "" || conf.zoneIDs == "" {
 		flag.Usage()
 		log.Error("required parameters missing")
 		return
 	}
 
 	services := strings.Split(conf.services, ",")
+	zones := strings.Split(conf.zoneIDs, ",")
 
 	cm, err := consul.NewConsulManager(
 		conf.addresses,
@@ -61,12 +62,14 @@ func main() {
 	go cm.Run()
 	defer cm.Stop()
 
-	dm, err := dns.NewDNSManager(
-		conf.zoneID,
-	)
-	if err != nil {
-		log.Error("create dns manager", "error", hclog.Fmt("%s", err))
-		return
+	dms := []dns.DNSManager{}
+	for _, zone := range zones {
+		dm, err := dns.NewDNSManager(zone)
+		if err != nil {
+			log.Error("create dns manager", "error", hclog.Fmt("%s", err))
+			return
+		}
+		dms = append(dms, *dm)
 	}
 
 	for range time.NewTicker(time.Duration(conf.interval) * time.Second).C {
@@ -81,36 +84,38 @@ func main() {
 				continue
 			}
 
-			entries, records, err := dm.GetDNSRecords(service)
-			if err != nil {
-				log.Error("get dns records", "service", service, "error", hclog.Fmt("%s", err))
-				continue
-			}
+			for _, dm := range dms {
+				entries, records, err := dm.GetDNSRecords(service)
+				if err != nil {
+					log.Error("get dns records", "service", service, "error", hclog.Fmt("%s", err))
+					continue
+				}
 
-			// ensure all consul endpoints are in dns
-			for _, endpoint := range endpoints {
-				if slices.Contains(entries, endpoint) {
-					log.Debug("add-to-dns", "service", service, "existing record", hclog.Fmt("%s", endpoint), "action", "none")
-				} else {
-					log.Info("add-to-dns", "service", service, "non existing record", hclog.Fmt("%s", endpoint), "action", "add")
-					err := dm.InsertDNSRecord(service, endpoint)
-					if err != nil {
-						log.Error("insert dns record", "service", service, "error", hclog.Fmt("%s", err))
-						continue
+				// ensure all consul endpoints are in dns
+				for _, endpoint := range endpoints {
+					if slices.Contains(entries, endpoint) {
+						log.Debug("add-to-dns", "service", service, "existing record", hclog.Fmt("%s", endpoint), "action", "none")
+					} else {
+						log.Info("add-to-dns", "service", service, "non existing record", hclog.Fmt("%s", endpoint), "action", "add")
+						err := dm.InsertDNSRecord(service, endpoint)
+						if err != nil {
+							log.Error("insert dns record", "service", service, "error", hclog.Fmt("%s", err))
+							continue
+						}
 					}
 				}
-			}
 
-			// deleting stale dns records
-			for _, entry := range entries {
-				if slices.Contains(endpoints, entry) {
-					log.Debug("clean-dns", "service", service, "existing endpoint", hclog.Fmt("%s", entry), "action", "none")
-				} else {
-					log.Info("clean-dns", "service", service, "non existing endpoint", hclog.Fmt("%s", entry), "action", "delete")
-					err := dm.DeleteDNSRecord(records[entry])
-					if err != nil {
-						log.Error("delete dns record", "service", service, "error", hclog.Fmt("%s", err))
-						continue
+				// deleting stale dns records
+				for _, entry := range entries {
+					if slices.Contains(endpoints, entry) {
+						log.Debug("clean-dns", "service", service, "existing endpoint", hclog.Fmt("%s", entry), "action", "none")
+					} else {
+						log.Info("clean-dns", "service", service, "non existing endpoint", hclog.Fmt("%s", entry), "action", "delete")
+						err := dm.DeleteDNSRecord(records[entry])
+						if err != nil {
+							log.Error("delete dns record", "service", service, "error", hclog.Fmt("%s", err))
+							continue
+						}
 					}
 				}
 			}
